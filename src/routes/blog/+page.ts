c5146bb089c17913f00b4a845f826ec43e738a23
meet-mistry <meetmistry0@outlook.com>
2022-12-29 19:22:00 +0530

    switch blog data fetch from js to ts

--- src/routes/blog/+page.ts
+++ src/routes/blog/+page.ts
@@ -1,9 +1,11 @@
 // @ts-nocheck
-export const load = async ({ fetch }) => {
+import type { PageLoad } from './$types';
+
+export const load = (async ({ fetch }) => {
     const response = await fetch(`/api/posts`)
     const posts = await response.json()
 
     return {
         posts
     }
-}
+}) satisfies PageLoad;
