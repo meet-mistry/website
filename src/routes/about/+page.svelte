19cc78db52a2dab83f5390b4ce8c724cc84f8d75
meet-mistry <117237721+meet-mistry@users.noreply.github.com>
2024-04-18 01:11:19 +0530

    Upgrade 1.1

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+    <title>About | Meet Mistry</title>
+    <meta name="description" content="About Me" />
+</svelte:head>
+
+<main>
+    <h1 class="page-title">About Me</h1>
+    <p>
+       WIP
+    </p>
+
+    <p>WIP</p>
+</main>
---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+    <title>Blog | Meet Mistry</title>
+    <meta name="description" content="About Me" />
+</svelte:head>
+
+<main>
+    <h1 class="page-title">Blog</h1>
+    
+    <p>WIP</p>
+</main>
